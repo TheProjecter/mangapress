0546c5433f3d97218f8b90d0fd402e05fa9ff660
ardath.ksheyna78 <ardath.ksheyna78@3bed9fe8-2492-7da9-cef2-bd0d4ed9c7e6>
2011-03-18 18:24:54 +0000

    Added comic templates section to mp-default theme. Updates to CSS and markup.

---
+++ branches/2.7/themes/mp-default/comics/latest-comic.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package Manga_Press
+ * @version $Id$
+ * @author Jess Green <jgreen@psy-dreamer.com>
+ */
+/**
+ * @package MangaPress_Bundled_Theme
+ * @subpackage Latest_Comic
+ * @version $Id$
+ * @author Jess Green <jgreen@psy-dreamer.com>
+ */
+global $mp_options;
+get_header(); ?>
+
+<div id="content" class="hfeed latest-comic">
+    <?php
+
+        $args = array(
+            'category__in'   => array($mp_options['latestcomic_cat']),
+            'posts_per_page' => '1',
+            'paged'          => get_query_var('paged'),
+        );
+
+        $comic_query = new WP_Query($args);
+
+    ?>
+
+    <?php if ($comic_query->have_posts()) : while($comic_query->have_posts()) : ?>
+    <?php $comic_query->the_post(); ?>
+
+    <?php $cats = wp_get_post_categories(get_the_ID()); ?>
+    <?php wp_comic_navigation($comic_query); ?>
+    <div id="post_<?php the_ID() ?>" <?php post_class('comic') ?>>
+        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+        <?php if (has_post_thumbnail()): ?>
+        <div class="comic">
+            <?php the_post_thumbnail('comic-page'); ?>
+        </div>
+        <?php endif; ?>
+
+        <div class="entry-content">
+            <?php the_content(); ?>
+        </div>
+
+        <?php endwhile; ?>
+        <?php comments_template( '', true ); ?>
+    <?php endif;?>
+    </div>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- branches/2.7/themes/mp-default/index.php
+++ branches/2.7/themes/mp-default/index.php
@@ -5,26 +5,28 @@
  * @version $Id$
  */
 /**
- * @package MangaPress
- * @subpackage MangaPress_Bundled_Theme
+ * @package MangaPress_Bundled_Theme
+ * @subpackage Default
  * @author Jess Green <jgreen@psy-dreamer.com>
  * @version $Id$
  */
 ?>
 <?php get_header(); ?>
 
-        <div id="content" class="hfeed">
-        <?php if (have_posts ()) : while(have_posts()) : the_post(); ?>
-            <div id="post_<?php the_ID() ?>" <?php post_class() ?>>
-                <h2 class="entry-title"><?php the_title(); ?></h2>
-                <div class="entry-content">
-                    <?php the_content();?>
-                </div>
-            </div>
-               
-        <?php endwhile; endif; ?>
+    <div id="content" class="hfeed">
 
+    <?php if (have_posts ()) : while(have_posts()) : the_post(); ?>
+        <div id="post_<?php the_ID() ?>" <?php post_class() ?>>
+            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <span class="meta"><?php the_date('M j Y') ?></span>
+            <div class="entry-content">
+                <?php the_content();?>
+            </div>
         </div>
-        <?php get_sidebar(); ?>
+
+    <?php endwhile; endif; ?>
+
+    </div>
+    <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
