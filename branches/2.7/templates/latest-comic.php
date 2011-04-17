3fdbd95629da2d9ec4280428ec83162a5d75e859
ardath.ksheyna78 <ardath.ksheyna78@3bed9fe8-2492-7da9-cef2-bd0d4ed9c7e6>
2011-04-17 16:08:50 +0000

    Added post thumbnail support to templates.

--- branches/2.7/templates/latest-comic.php
+++ branches/2.7/templates/latest-comic.php
@@ -37,6 +37,11 @@ get_header(); ?>
             
         <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
                 <div class="entry_content">
+                    <?php if (has_post_thumbnail()): ?>
+                    <div class="comic">
+                        <?php the_post_thumbnail('comic-page'); ?>
+                    </div>
+                    <?php endif; ?>
                     <?php the_content(); ?>
                 </div>
 
