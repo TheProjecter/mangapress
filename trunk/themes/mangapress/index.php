ee090fe2684168795fc694a66ee6c766fd240855
ardath.ksheyna78 <ardath.ksheyna78@3bed9fe8-2492-7da9-cef2-bd0d4ed9c7e6>
2012-02-06 03:39:35 +0000

    Moved Options page to separate class. Added uninstall routine. Added themes directory and files. Removed old screenshots in preparation for new screenshots.

---
+++ trunk/themes/mangapress/index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package Manga_Press
+ * @subpackage MangaPress_Bundled_Theme
+ * @author Jess Green <jgreen@psy-dreamer.com>
+ * @version $Id$
+ */
+/**
+ * @package MangaPress_Bundled_Theme
+ * @subpackage Default
+ * @author Jess Green <jgreen@psy-dreamer.com>
+ * @version $Id$
+ */
+?>
+<?php get_header(); ?>
+
+    <div id="content" class="hfeed">
+
+    <?php /* if (have_posts()) : while(have_posts()) : the_post(); ?>
+        
+        <div id="post-<?php the_ID() ?>" <?php post_class() ?>>
+            
+            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            
+            <span class="meta"><?php the_time('F jS') ?> | Posted In: <?php the_category(', ') ?></span>
+            
+            <div class="entry-content">
+                <?php the_content();?>
+
+                <span class="meta"><?php comments_popup_link() ?></span>
+            </div>
+            
+        </div> <!-- #post-<?php the_ID() ?> -->
+
+    <?php endwhile; ?>
+    <?php endif; */?>
+
+    </div>
+    <?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
