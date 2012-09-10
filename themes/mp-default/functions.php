a1016d3861b9130fe3bf511f5b494813e9164956
jgreen <jgreen@psy-dreamer.com>
2012-09-10 02:32:31 +0000

    Adding templates for default theme. Correcting issues with template processing.

--- themes/mp-default/functions.php
+++ themes/mp-default/functions.php
@@ -21,3 +21,16 @@ function disable_options($options)
     return $options;
     
 }
+
+add_action('wp_enqueue_scripts', 'mangapress_theme_load_twentyeleven_css');
+/**
+ * Load the stylesheet from the TwentyEleven Theme
+ * @return void
+ */
+function mangapress_theme_load_twentyeleven_css()
+{
+    $src = WP_CONTENT_URL . '/themes/twentyeleven/style.css';
+    wp_register_style('twentyeleven', $src, null, MP_VERSION);
+    
+    wp_enqueue_style('twentyeleven');
+}
